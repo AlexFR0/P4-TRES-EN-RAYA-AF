63f7376b36da128090d3a7b2fb3bd6381a5a326b
AlexFR0 <afrancesretamero50@gmail.com>
2024-12-12 18:39:24 +0100

    PARTE1
    En esta parte declaramos una STRING para escribir los nombres de los 2 jugadores y se almacenen.
    
    Creamos una matriz con char para simbolizar el juego 3X3.

--- P4-TRES-EN-RAYA/src/packageRaya/P4TRESENRAYA.java
+++ P4-TRES-EN-RAYA/src/packageRaya/P4TRESENRAYA.java
@@ -1,10 +1,32 @@
 package packageRaya;
+import java.util.Scanner;
 
 public class P4TRESENRAYA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//PARTE1
+        Scanner scanner = new Scanner(System.in);
+		
+		// Solicitar los nombres de los jugadores
+        System.out.print("Ingrese el nombre del jugador 1: ");
+        String jugador1 = scanner.nextLine();
+        
+        System.out.print("Ingrese el nombre del jugador 2: ");
+        String jugador2 = scanner.nextLine();
+        
+        char[][] tablero = {
+                {'-', '-', '-'},
+                {'-', '-', '-'},
+                {'-', '-', '-'}
+            };
+		
+		
+		
+		
+		
+		
 	}
 
 }
